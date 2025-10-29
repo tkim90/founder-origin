a358ed6cd27ad128f20dbe391c05b14a119e1194
Tae Kim <taehnkim@gmail.com>
2025-10-28 22:16:00 -0700

    init

---
+++ app/list.ts
@@ -0,0 +1,12 @@
+export const LIST = [
+  {
+    name: "Pieter Levels",
+    revenue: "PhotoAI.com, $140k/mo",
+    story: "Pieter Levels, the founder of Nomad List, started the project in 2014 while traveling as a digital nomad. He initially created a simple Google spreadsheet compiling data on affordable cities with good internet and amenities for remote workers, as this information was scattered and hard to find. He shared the spreadsheet publicly on Twitter, which quickly went viral among the growing digital nomad community. This allowed him to crowdsource additional data from users, who contributed their own insights on cities."
+  },
+  {
+    name: "Test 2",
+    revenue: "",
+    story: "this is the second story",
+  },
+]
