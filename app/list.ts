5cf68248f2b2690e328c2b81f7ccd4bcdafb8b9c
Tae Kim <taehnkim@gmail.com>
2025-10-28 22:41:09 -0700

    styling to light

--- app/list.ts
+++ app/list.ts
@@ -1,12 +1,8 @@
 export const LIST = [
   {
-    name: "Pieter Levels",
-    revenue: "PhotoAI.com, $140k/mo",
-    story: "Pieter Levels, the founder of Nomad List, started the project in 2014 while traveling as a digital nomad. He initially created a simple Google spreadsheet compiling data on affordable cities with good internet and amenities for remote workers, as this information was scattered and hard to find. He shared the spreadsheet publicly on Twitter, which quickly went viral among the growing digital nomad community. This allowed him to crowdsource additional data from users, who contributed their own insights on cities."
-  },
-  {
-    name: "Test 2",
-    revenue: "",
-    story: "this is the second story",
+    name: "Surge",
+    revenue: "$1,000,000,0000 ($1B)",
+    story: "Founder Edwin Chen, a former Google and Meta engineer, leveraged his knowledge of pain points and his connections in the industry to validate the product and secure initial customers among well-known names such as Airbnb and Twitch. Surge's earliest projects focused on solving core problems for these companies: improving search, recommendation engines, and content moderation through higher quality, expert-matched data labeling.",
+    url: "https://www.surgehq.ai/",
   },
 ]
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,36 @@
+import Link from 'next/link';
 import { LIST } from "./list";
+
+const COPY_HEADER = 'A curated list of examples of how companies got their first few customers';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col py-32 px-16 bg-white dark:bg-black sm:items-start">
-        Founder Origins
+    <div className="flex min-h-screen items-center justify-center bg-[#EEEEE8] font-sans">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col py-32 px-16 bg-[#EEEEE8] sm:items-start">
+
+        <span className="text-3xl text-black">Founder Origins</span>
         <div className="flex gap-6">
-          <h1 className="text-3xl font-semibold tracking-tight text-black dark:text-zinc-50">
-            A curated list of examples of how companies got their first few customers
+          <h1 className="text-2xl tracking-tight text-black">
+            {COPY_HEADER}
           </h1>
         </div>
 
           {LIST.map(v => (
             <div 
-              className="flex flex-col my-6 border-2 border border-white rounded-xl p-8"
+              className="flex flex-col my-6"
               key={`${v.name}`}
             >
-              <span className="text-2xl">{v.name}</span>
-              <span className="text-xl text-white/80 font-medium mb-4">{v.revenue}</span>
-              <span className="text-xl">{v.story}</span>
+              { v.url ? (
+                <Link href={v.url} className="text-2xl text-black underline" target="_blank">
+                  {v.name}
+                </Link>
+              ) : (
+                <span className="text-2xl text-black">
+                  {v.name}
+                </span>
+              ) }
+              <span className="text-xl text-gray-800 font-medium mb-4">{v.revenue}</span>
+              <span className="text-xl text-gray-800">{v.story}</span>
             </div>
           ))}
       </main>
